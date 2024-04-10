5fee0d92f3ca9f4d503778b7b2493acac3a95a14
usuario <usuario@usuario>
2024-04-10 10:37:35 +0200

    añadir los demas metodos

--- src/main/java/com/ejemplos/models/service/SenderoServiceImpl.java
+++ src/main/java/com/ejemplos/models/service/SenderoServiceImpl.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ejemplos.models.dao.IMunicipioDao;
 import com.ejemplos.models.dao.ISenderoDao;
-import com.ejemplos.models.entity.Municipio;
 import com.ejemplos.models.entity.Sendero;
 
 @Service
@@ -16,23 +14,23 @@ public class SenderoServiceImpl {
 	
 	@Autowired
 	private ISenderoDao senderoDao;
-	@Autowired
-	private IMunicipioDao municipioDao;
+
 
 	@Transactional(readOnly=true)
 	public List<Sendero> TSendero(){
 		return (List<Sendero>)senderoDao.findAll();
 	}
-	@Transactional(readOnly=true)
-	public List<Municipio> TMuni(){
-		return (List<Municipio>)municipioDao.findAll();
-	}
+
 	@Transactional
 	public void save(Sendero sendero) {
 		senderoDao.save(sendero);
 	}
-	@Transactional
-	public void save(Municipio muni) {
-		municipioDao.save(muni);
+	
+	public Sendero findOne(String id) {
+		return senderoDao.findById(id).orElse(null);
+	}
+	
+	public void delete(String id) {
+		senderoDao.deleteById(id);
 	}
 }
